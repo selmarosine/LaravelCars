6f9e5fc75f48692d608af263848c5a3f29f1060c
selmarosine <selma.gronsten@hotmail.se>
2024-02-26 11:25:04 +0100

    database

---
+++ cars/database/migrations/2024_02_26_095950_create_cars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            //$table->primary('id');
+            //$table->timestamps();
+            $table->string('regnr')->unique();
+            $table->string('namn');
+            $table->integer('pris');
+            $table->date('datum');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
