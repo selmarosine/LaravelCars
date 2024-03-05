ab470687522d87dd89f489811a319779e599be27
selmarosine <selma.gronsten@hotmail.se>
2024-03-05 15:01:43 +0100

    working on LoginController

--- cars/resources/views/index.blade.php
+++ cars/resources/views/index.blade.php
@@ -8,5 +8,18 @@
         <input type="text" id="regNo">
         <button type="submit" name="submitReg">Submit</button>
     </form>
+
+    <div style="margin-top: 55px;">
+        <h3>Already have an account? Sign in:</h3>
+        <form method="POST" action="{{ url('/login') }}">
+            @csrf
+            <label for="name">Name:</label>
+            <input type="text" id="namn" name="name">
+            <div><label for="password">Password:</label>
+                <input name="password" type="password" id="password">
+            </div>
+            <div><button type="submit" name="submitLogin">Log in</button></div>
+        </form>
+    </div>
 </main>
 @include('nav.footer')
--- cars/routes/web.php
+++ cars/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('login')->middleware('guest');
+
+Route::post('login', LoginController::class);
