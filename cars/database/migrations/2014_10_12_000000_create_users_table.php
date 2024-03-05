8280d31416158a07a351835987aaa250ea26e134
selmarosine <selma.gronsten@hotmail.se>
2024-03-05 14:23:39 +0100

    added LoginController

--- cars/database/migrations/2014_10_12_000000_create_users_table.php
+++ cars/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             /* $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable();*/
             $table->string('password');
-            $table->rememberToken();*/
+            $table->rememberToken();
             $table->timestamps();
         });
     }
