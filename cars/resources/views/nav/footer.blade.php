fe46ee65d1551e63a04c7b6ece30301dbaef1902
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2024-03-07 09:09:05 +0100

    freestyle styling

--- cars/resources/views/nav/footer.blade.php
+++ cars/resources/views/nav/footer.blade.php
@@ -1,3 +1,4 @@
+<footer class="mainFooter">We destroy all your things! Call us today: DEST-101-444-56</footer>
 </body>
 
 </html>
--- cars/resources/views/nav/header.blade.php
+++ cars/resources/views/nav/header.blade.php
@@ -5,8 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <title>Scrap n' Destroy</title>
 </head>
-<header>My Logo</header>
+<header class="mainHeader">
+    <div class="headerTextContainer">
+        <h1>Lil' Joe's Funeral Home</h1>
+        <h4>- A final resting place for your best friend -</h4>
+    </div>
+</header>
 
 <body>
