2f30e30f9d97425843425a446d14d4168ce52b73
selmarosine <selma.gronsten@hotmail.se>
2024-03-11 21:21:52 +0100

    more styling

--- cars/resources/views/index.blade.php
+++ cars/resources/views/index.blade.php
@@ -6,12 +6,15 @@ use Illuminate\Support\Facades\DB;
 <h1 class="indexTitle">
     Car Destruction
 </h1>
+
 <main class="indexMain">
-    <form action="{{ route('submit.form') }}" method="POST" class="indexReg">
+    <div class="indexReg">
+    <h3 class="indexH3">New here?</h3>
+    <form action="{{ route('submit.form') }}" method="POST">
         @csrf
-        <label for="regNo">Enter Reg Number:</label>
+        <div><label for="regNo">Enter Reg Number:</label></div>
         <input type="text" id="regNo" name="regNo">
-        <button type="submit" name="submitReg">Submit</button>
+        <div><button type="submit" name="submitReg">Continue</button></div>
         @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -22,15 +25,16 @@ use Illuminate\Support\Facades\DB;
         </div>
         @endif
     </form>
+    </div>
 
     <div class="indexLogin">
-        <h3>Already have an account? Sign in:</h3>
+        <h3 class="indexH3">Login:</h3>
         <form method="POST" action="{{ url('/login') }}">
             @csrf
-            <label for="name">Name:</label>
+            <div><label for="name">Name:</label></div>
             <input type="text" id="namn" name="name">
-            <div><label for="password">Password:</label>
-                <input name="password" type="password" id="password">
+            <div><label for="password">Password:</label></div>
+            <div><input name="password" type="password" id="password">
             </div>
             <div><button type="submit" name="submitLogin">Log in</button></div>
         </form>
